0a8e7109ed02c8d9f7a60cc45455bc7baf12dcc9
Jérémie Bellanger <bellanger.jeremie@gmail.com>
2022-06-28 18:29:35 +0200

    update model

---
+++ src/extensions/users-permissions/strapi-server.js
@@ -0,0 +1,22 @@
+module.exports = plugin => {
+  const sanitizeOutput = (user) => {
+    const {
+      password, resetPasswordToken, confirmationToken, ...sanitizedUser
+    } = user; // be careful, you need to omit other private attributes yourself
+    return sanitizedUser;
+  };
+
+  plugin.controllers.user.me = async (ctx) => {
+    if (!ctx.state.user) {
+      return ctx.unauthorized();
+    }
+    const user = await strapi.entityService.findOne(
+      'plugin::users-permissions.user',
+      ctx.state.user.id,
+      { populate: ['modules', 'modules.users_permissions_users'] }
+    );
+
+    ctx.body = sanitizeOutput(user);
+  };
+  return plugin;
+};
